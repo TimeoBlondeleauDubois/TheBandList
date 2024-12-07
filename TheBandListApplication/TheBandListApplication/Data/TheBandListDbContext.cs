fac3aa3e3c5ecb7e070177c58c9494da77802ecd
Blondeleau <timeo.blondeleaudubois@viacesi.fr>
2024-12-07 22:44:40 +0100

    Add Db connection

---
+++ TheBandListApplication/TheBandListApplication/Data/TheBandListDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using DotNetEnv;
+
+namespace TheBandListApplication.Data
+{
+    internal class TheBandListDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            Env.Load();
+
+            string host = Environment.GetEnvironmentVariable("DB_HOST");
+            string port = Environment.GetEnvironmentVariable("DB_PORT");
+            string database = Environment.GetEnvironmentVariable("DB_NAME");
+            string username = Environment.GetEnvironmentVariable("DB_USERNAME");
+            string password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+            string connectionString = $"Host={host};Port={port};Database={database};Username={username};Password={password};SslMode=Require;Trust Server Certificate=true;";
+
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+
+        //ici on déclare les DbSet pour chaque table de la base de données
+    }
+}
