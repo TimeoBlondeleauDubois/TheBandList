3d5725cf0ed6f1fe848f2657d0d358bf389eb3ea
Blondeleau <timeo.blondeleaudubois@viacesi.fr>
2024-12-10 22:52:41 +0100

    Backend Home Done

--- TheBandList/Data/TheBandListDbContext.cs
+++ TheBandList/Data/TheBandListDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using TheBandListApplication.Data;
 
 namespace TheBandList.Data
 {
@@ -8,5 +9,33 @@ namespace TheBandList.Data
         public TheBandListDbContext(DbContextOptions<TheBandListDbContext> options)
         : base(options)
         { }
+
+        public DbSet<Utilisateur> Utilisateurs { get; set; }
+        public DbSet<Niveau> Niveaux { get; set; }
+        public DbSet<NiveauDifficulteRate> NiveauxDifficulteRates { get; set; }
+        public DbSet<DifficulteFeature> DifficulteFeatures { get; set; }
+        public DbSet<Pack> Packs { get; set; }
+        public DbSet<PackNiveau> PackNiveaux { get; set; }
+        public DbSet<ReussitePack> ReussitesPack { get; set; }
+        public DbSet<Classement> Classements { get; set; }
+        public DbSet<CreateurNiveau> CreateursNiveaux { get; set; }
+        public DbSet<ReussiteNiveau> ReussitesNiveaux { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PackNiveau>()
+                .HasKey(pn => new { pn.PackId, pn.NiveauId });
+
+            modelBuilder.Entity<ReussitePack>()
+                .HasKey(rp => new { rp.UtilisateurId, rp.PackId });
+
+            modelBuilder.Entity<CreateurNiveau>()
+                .HasKey(cn => new { cn.CreateurId, cn.NiveauId });
+
+            modelBuilder.Entity<ReussiteNiveau>()
+                .HasKey(rn => new { rn.UtilisateurId, rn.NiveauId });
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
